92d45284f8f1de4ac0ae1ae8a1b4770d89be98fa
Takahiro Nakamura <taka-nakamura@cresco.co.jp>
2018-06-08 19:25:01 +0900

    change framework from chainer to pytorch

---
+++ cnn/single/dataset.py
@@ -0,0 +1,69 @@
+# -*- coding:utf-8 -*-
+from collections import defaultdict
+import glob
+import itertools
+import numpy
+import pandas
+from PIL import Image
+import os
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+import torch
+from torch.autograd import Variable
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+import re
+
+if os.name == 'nt':
+    import win_unicode_console
+
+class SaikoroImageDataSet(Dataset):
+    def __init__(self, X=None, y=None, train=True):
+        self.augmentor = transforms.Compose([transforms.RandomHorizontalFlip(),
+                                             transforms.RandomRotation(degrees=30),
+                                             transforms.RandomVerticalFlip()])
+        self.loader = transforms.Compose([transforms.ToTensor()])
+        self.train = train
+
+        if X is not None and y is not None:
+            self.X = X
+            self.y = y
+        else:
+            files = "images/*.jpg"
+            files = glob.glob(files)
+            self.X = files
+            self.y = [int(file.split(".jpg")[0].split("_")[1])-1 for file in files]
+            print(self.X)
+            print(self.y)
+
+    def train_test_split(self, train_split=0.8):
+        X_train, X_test, y_train, y_test = train_test_split(
+            self.X, self.y, test_size=1-train_split, random_state=51
+        )
+        return (SaikoroImageDataSet(X_train, y_train, train=True),
+                SaikoroImageDataSet(X_test, y_test, train=False))
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, idx):
+        image_path = self.X[idx]
+        label = self.y[idx]
+        image = self.load_image(image_path)
+        return image, torch.from_numpy(numpy.expand_dims(label, axis=0))
+
+    def load_image(self, image_name):
+        """load image, returns cuda tensor"""
+        image = Image.open(image_name)
+        image = image.convert('RGB')
+        if self.train:
+            image = self.augmentor(image)
+        image = self.loader(image).float()
+        return image / 255
+
+
+if __name__ == "__main__":
+    if os.name == 'nt':
+        win_unicode_console.enable()
+
+    SaikoroImageDataSet()
--- cnn/single/train.py
+++ cnn/single/train.py
@@ -1,54 +1,184 @@
-import chainer
-from chainer import links as L
-from chainer import training
-from chainer.training import extensions
-from gen_image_dataset import gen_image_dataset
+# -*- using:utf-8 -*-
+import torch
+from torch import nn
+from torch import optim
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+import torch.nn.functional as F
+import torchvision
+from torchvision import transforms
+from torchvision.models.resnet import resnet18, resnet50
+from tensorboardX import SummaryWriter
+from dataset import SaikoroImageDataSet
+import numpy as np
+import os
 
-from models.CNN import CNN
+import win_unicode_console
+from memory_profiler import profile
 
-def main(batchsize=10, gpu=-1, epoch = 100, resume = False):
-    train, test = gen_image_dataset()
+from pretrainedmodels import inceptionv4
 
-    train_iter = chainer.iterators.SerialIterator(train, batchsize)
-    # test_iter = chainer.iterators.SerialIterator(test, batchsize,
-    #                                              repeat=False, shuffle=False)
-    test_iter = chainer.iterators.SerialIterator(test, 1,
-                                                 repeat=False, shuffle=False)
 
-    model = L.Classifier(CNN())
+class NetResnet50(torch.nn.Module):
+    def __init__(self):
+        super(NetResnet50, self).__init__()
+        self.cnn = nn.Sequential(*list(resnet50(pretrained=True).children())[:-2])
+        # self.cnn = resnet50(pretrained=True)
 
-    if gpu >= 0:
-        chainer.cuda.get_device(gpu).use()
-        model.to_gpu()
+        self.fc = nn.Sequential(
+            nn.Linear(2048 * 4 * 5, 126),
+            nn.ReLU(),
 
-    opt = chainer.optimizers.Adam()
-    opt.setup(model)
+            nn.Dropout(0.5),
+            nn.Linear(126, 6),
+            nn.ReLU(),
+        )
 
-    updater = training.StandardUpdater(train_iter, opt, device=gpu)
-    trainer = training.Trainer(updater, (epoch, 'epoch'), out='./result')
+    def forward(self, X):
+        X = self.cnn(X)
+        X = X.view(-1, 2048 * 4 * 5)
+        return self.fc(X)
 
-    trainer.extend(extensions.Evaluator(test_iter, model, device=gpu))
+    def setCNNTrainable(self, trainable):
+        if trainable:
+            print("train CNN")
+            for p in self.cnn.parameters():
+                p.requires_grad = True
+        else:
+            print("do not train CNN")
+            for p in self.cnn.parameters():
+                p.requires_grad = False
 
-    trainer.extend(extensions.dump_graph('main/loss'))
 
-    trainer.extend(extensions.snapshot(), trigger=(epoch, 'epoch'))
-    trainer.extend(extensions.snapshot_object(model, 'snapshot_{.updater.iteration}'))
+def main():
+    # workaround a bug for windows
+    # https://bugs.python.org/issue32245
+    win_unicode_console.enable()
 
-    trainer.extend(extensions.LogReport())
+    dataset = SaikoroImageDataSet()
 
-    trainer.extend(extensions.PrintReport(
-        ['epoch', 'main/loss', 'validation/main/loss',
-         'main/accuracy', 'validation/main/accuracy']))
+    train_dataset, test_dataset = dataset.train_test_split(train_split=0.8)
 
-    trainer.extend(extensions.ProgressBar())
+    model = NetResnet50()
+    criterion = nn.CrossEntropyLoss()
+    # criterion = nn.L1Loss()
 
-    if resume:
-        chainer.serializers.load_npz(resume, trainer)
+    if torch.cuda.is_available():
+        print('use cuda.')
+        print('loading model.')
+        model = model.cuda()
+        print('loading criterion.')
+        criterion = criterion.cuda()
+        print('loading finished.')
 
-    trainer.run()
+    for trainable, epochs in [(False, 50), (True, 100)]:
+    # for trainable, epochs in [(True, 200)]:
+    # for trainable in [True]:
+        model.setCNNTrainable(trainable)
 
-    chainer.serializers.save_npz('./saikoro_model.npz', model)
+        optimizer = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=5e-5, weight_decay=1e-7)
+        # optimizer = optim.SGD(filter(lambda p: p.requires_grad, model.parameters()), lr=1e-3, momentum=0.9)
+        train_test(model, criterion, optimizer, train_dataset, test_dataset, epochs)
 
+
+def train_test(model, criterion, optimizer, train_dataset, test_dataset, epochs, k=0):
+    # model path to save weights
+    model_path = 'models/model_{}.pth'.format(k)
+    # tensorboard summary writer
+    writer_train = SummaryWriter('./logs/train/{}'.format(k))
+    writer_test = SummaryWriter('./logs/test/{}'.format(k))
+    # calculate best test accuracy to save best model weights
+    best_accuracy = 0.0
+
+    for epoch in range(epochs):
+        # pytorch bug: memory leaks when shuffle is True.
+        # train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True, num_workers=4)
+        train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True, num_workers=0)
+        test_dataloader = DataLoader(test_dataset, batch_size=32, shuffle=False, num_workers=0)
+
+        train_loss, train_accuracy = process(model, train_dataloader, optimizer, criterion, train=True)
+        test_loss, test_accuracy = process(model, test_dataloader, optimizer, criterion, train=False)
+
+        print('[%.2d epoch %.3d/%.3d] loss(train): %.5f, accuracy(train) %.5f, loss(val) %.5f, accuracy(val) %.5f' %
+              (k, epoch + 1, epochs, train_loss, train_accuracy, test_loss, test_accuracy))
+
+        writer_train.add_scalar('loss', train_loss, epoch)
+        writer_test.add_scalar('loss', test_loss, epoch)
+        writer_train.add_scalar('acc', train_accuracy, epoch)
+        writer_test.add_scalar('acc', test_accuracy, epoch)
+
+        if test_accuracy > best_accuracy:
+            print("accuracy improved. %.3f -> %.3f" % (best_accuracy, test_accuracy))
+            best_accuracy = test_accuracy
+            if test_accuracy >= 0.6:
+                print("save model to {}".format(model_path))
+                model_save(model, model_path)
+                writer_test.add_scalar('best_acc', test_accuracy, epoch)
+
+
+def process(model, dataloader, optimizer, criterion, train=True):
+    loss_list = []
+    correct_list = torch.Tensor([])
+    if torch.cuda.is_available():
+        correct_list = correct_list.cuda()
+
+    for batch_iter, batch_data in enumerate(dataloader):
+        # print(str(batch_data).encode('cp932', 'ignore'))
+        image = batch_data[0]
+        label = batch_data[1]
+
+        image = Variable(image, requires_grad=False)
+        label = Variable(label, requires_grad=False)
+
+        if torch.cuda.is_available():
+            image = image.cuda()
+            label = label.cuda()
+
+        if train:
+            model.train()
+        else:
+            model.eval()
+
+        # forward + backward + optimize
+        outputs = model.forward(image)
+
+        # https://github.com/pytorch/pytorch/issues/3670
+        # label should be 1d tensor.
+        outputs = outputs.squeeze(1)
+        label = label.squeeze(1)
+
+        # for windows
+        label = label.long()
+
+        loss = criterion(outputs, label)
+
+        if train:
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        # for classification
+        # argmax
+        _, predicted = torch.max(outputs.data, 1)
+        correct = (predicted == label.data).float()
+
+        loss_list.append(loss.data[0])
+        # print(predicted)
+        # print(correct)
+        # print(correct_list.size())
+        correct_list = torch.cat([correct_list, correct], 0)
+
+    loss_ave = np.mean(loss_list)
+    accuracy_ave = correct_list.sum() / correct_list.size()[0]
+
+    return loss_ave, accuracy_ave
+
+
+def model_save(model, path):
+    dirname = os.path.dirname(path)
+    if not os.path.exists(dirname):
+        os.makedirs(dirname)
+    torch.save(model.state_dict(), path)
 
 if __name__ == '__main__':
     main()
